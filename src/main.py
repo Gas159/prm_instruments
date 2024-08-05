cc7b6a5f4cd3b3c93744c2637c1e3e04c85cee98
gas <pankot222@gmail.com>
2024-08-05 17:50:14 +0500

    add CORS

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from contextlib import asynccontextmanager
-
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 import uvicorn
+
+
 from api import router as api_router
 from config import settings
 from core.models import db_helper
@@ -15,12 +17,34 @@ async def lifespan(app: FastAPI):
     print("dispose engine")
     await db_helper.dispose()
 
-#1
+
 main_app = FastAPI(lifespan=lifespan)
 main_app.include_router(
     api_router,
     prefix=settings.api.prefix,
     # mainApp.mount("/app", app)  # your app routes will now be /app/{your-route-here}
+)
+
+origins = [
+    # "http://localhost:3000",
+    # "https://car-service-18635.web.app",
+    # "https://*",
+    # "http://*",
+    "*",
+]
+
+main_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=[
+        "Content-Type",
+        "Set-Cookie",
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Origin",
+        "Authorization",
+    ],
 )
 
 
@@ -31,8 +55,5 @@ async def root():
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:main_app",
-        host=settings.run.host,
-        port=settings.run.port,
-        reload=True
+        "main:main_app", host=settings.run.host, port=settings.run.port, reload=True
     )
