078faa99d4d5c508b0900da1ec7f64a67fcc72fe
gas <pankot222@gmail.com>
2024-08-05 18:44:35 +0500

    upd config.py

--- src/config.py
+++ src/config.py
@@ -4,35 +4,33 @@ from pydantic import PostgresDsn
 
 
 class RunConfig(BaseModel):
-	host: str = '0.0.0.0'
-	port: int = 8001
+    host: str = "0.0.0.0"
+    port: int = 8001
 
 
 class ApiPrefix(BaseModel):
-	prefix: str = '/api'
+    prefix: str = "/api"
 
 
 # url: str = 'postgresql+asyncpg://gas:123@localhost:5432/shop'
 class DatabaseConfig(BaseModel):
-	url: PostgresDsn
-	#url: str = 'postgresql+asyncpg://gas:123@localhost:5432/shop'
-	echo: bool = False
-	echo_pool: bool = False
-	pool_size: int = 50
-	max_overflow: int = 10
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
 
 
 class Settings(BaseSettings):
-	model_config = SettingsConfigDict(
-		env_file=('.env', '.env.template'),
-		case_sensitive=False,
-		env_nested_delimiter="__",
-		env_prefix='FASTAPI__'
-	)
-	run: RunConfig = RunConfig()
-	api: ApiPrefix = ApiPrefix()
-	#db: DatabaseConfig = DatabaseConfig()
-	db: DatabaseConfig
+    model_config = SettingsConfigDict(
+        env_file=(".env", ".env.template"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="FASTAPI__",
+    )
+    run: RunConfig = RunConfig()
+    api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig
 
 
 settings = Settings()
