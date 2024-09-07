2441631b6cc4aca95f69a6db364a9f32228eedd7
gas <pankot222@gmail.com>
2024-09-07 18:04:45 +0500

    refact: resolve all issues in test2.py

--- src/z_developer/test2.py
+++ src/z_developer/test2.py
@@ -18,8 +18,8 @@ class A:
     # def __eq__(self, other):
     #     return isinstance(other, A)  # Сравнение по типу
 
-    def __str__(self):
-        return "A"
+    # def __str__(self):
+    #     return "A"
 
 
 class B:
@@ -34,41 +34,46 @@ class B:
         return "B"
 
 
-#
+print(A())
+print(B())
+print(A() == B())
+print()
 var = {A(): 1, B(): 2}
 print(var)
-print(var[A()])  # Вывод: 1
+# print(var[A()])  # Вывод: 1
 # print(var[B()], var[A()])  # Вывод: 2
 
 
 #
 # ---
 # slot = type(A, __slot__, ('a','b'))
+
 #
-# #
-# class A:
-#     __slots__ = ("a", "b")
-#
-#     def __init__(self, a, b):
-#         self.a = a
-#         self.b = b
-#         # self.c = c
-#
-#
-# class B(A):
-#     __slots__ = ("c",)
-#
-#     def __init__(self, a, b, c):
-#         self.c = c
-#         super().__init__(a, b)
-#
-#     def __str__(self):
-#         return f"B: [{self.a}] [{self.b}] [{self.c}]"
-#
-#
-# # a = A(1, 2, 3)
-# # print(type(a), a.a, a.b, dir(a))
-# b = B(1,2,3)
-# print(B(1, 2, 3))
+class A:
+    __slots__ = ("a", "b")
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        # self.c = c
+
+
+class B(A):
+    __slots__ = ("c",)
+
+    def __init__(self, a, b, c):
+        self.c = c
+        super().__init__(a, b)
+
+    def __str__(self):
+        return f"B: [{self.a}] [{self.b}] [{self.c}]"
+
+
+# a = A(1, 2, 3)
+# print(type(a), a.a, a.b, dir(a))
+b = B(1,2,3)
+print(B(1, 2, 3))
+print(B.__slots__)
+print(A.__slots__)
 # #
 # # raise C
