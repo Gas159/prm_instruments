35a5942a6881d1ce6e260413d6e24cd672b56313
gas <pankot222@gmail.com>
2024-10-24 00:25:31 +0500

    fix: bag with fstapi_users

--- src/config.py
+++ src/config.py
@@ -81,7 +81,7 @@ class AuthJWT(BaseModel):
     public_key_path: Path = BASE_DIR / "auth_jwt" / "certs" / "jwt-public.pem"
     # public_key_path: Path = BASE_DIR / "auth_jwt/certs/jwt-public.pem"
     algorithm: str = "RS256"
-    access_token_expires_minutes: int = 15
+    access_token_expires_minutes: int = 3600
     # access_token_expires_minutes: int = 3
 
 
