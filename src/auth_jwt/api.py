7d30f869912e3664189fb8d47f83ce5131d02cbe
gas <pankot222@gmail.com>
2024-10-31 17:05:03 +0500

    feature: add refresh token and validate

--- src/auth_jwt/api.py
+++ src/auth_jwt/api.py
@@ -1,7 +1,15 @@
 from fastapi import APIRouter, Depends
 
-from auth_jwt.helpers import create_access_token, create_refresh_token
-from auth_jwt.jwt_auth import http_bearer, validate_auth_user, get_current_token_payload, get_current_active_auth_user
+from auth_jwt.helpers import create_access_token, create_refresh_token, REFRESH_TOKEN_TYPE
+from auth_jwt.jwt_auth import (
+    http_bearer,
+    validate_auth_user,
+    get_current_token_payload,
+    get_current_active_auth_user,
+    get_current_auth_user_for_refresh,
+    get_auth_user_from_token_of_type,
+    UserGetterFromToken,
+)
 from auth_jwt.schemas import UserAuthJWTSchema, TokenInfoSchema
 
 router = APIRouter(
@@ -20,8 +28,12 @@ def auth_user_login_jwt(
     return TokenInfoSchema(access_token=access_token, refresh_token=refresh_token, token_type="Bearer")
 
 
-@router.post("/refresh/", response_model=TokenInfoSchema, response_model_exclude_none=True)
-def auth_refresh_jwt():
+@router.post("/refresh", response_model=TokenInfoSchema, response_model_exclude_none=True)
+def auth_refresh_jwt(
+    user: UserAuthJWTSchema = Depends(get_current_auth_user_for_refresh),
+    # user: UserAuthJWTSchema =Depends( get_auth_user_from_token_of_type(REFRESH_TOKEN_TYPE)),
+    # user: UserAuthJWTSchema = Depends(UserGetterFromToken(REFRESH_TOKEN_TYPE)),
+) -> TokenInfoSchema:
     access_token = create_access_token(user)
     return TokenInfoSchema(access_token=access_token)
 
--- src/auth_jwt/jwt_auth.py
+++ src/auth_jwt/jwt_auth.py
@@ -53,7 +53,8 @@ def validate_token_type(
     payload: dict,
     token_type: str,
 ) -> bool:
-    if current_token_type := payload.get(TOKEN_TYPE_FIELD) == token_type:
+    current_token_type = payload.get(TOKEN_TYPE_FIELD)
+    if current_token_type == token_type:
         return True
     raise HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
@@ -68,7 +69,7 @@ def get_current_token_payload(
 ) -> UserAuthJWTSchema:
     # logger.debug("Token: %s", credentials)  # {'scheme': 'Bearer', 'credentials': '123'}
     # token = credentials.credentials
-    logger.debug("Token: %s", token)  # 123 - access token
+    logger.debug("Token: %s, type: %s", token, token)  # 123 - access token
     try:
         payload = decode_jwt(token=token)
     except InvalidTokenError as e:
@@ -83,23 +84,33 @@ def get_user_by_token_sub(payload: dict) -> UserAuthJWTSchema:
     return user
 
 
-def get_current_auth_user(
-    payload: dict = Depends(get_current_token_payload),
-) -> UserAuthJWTSchema:
-    validate_token_type(payload=payload, token_type=ACCESS_TOKEN_TYPE)
-    return get_user_by_token_sub(payload)
+def get_auth_user_from_token_of_type(token_type: str):  # 1
+    def get_auth_user_from_token(payload: dict = Depends(get_current_token_payload)) -> UserAuthJWTSchema:
+
+        validate_token_type(payload=payload, token_type=token_type)
+        return get_user_by_token_sub(payload)
+
+    return get_auth_user_from_token
 
 
-def get_current_auth_user_for_refresh(
-    payload: dict = Depends(get_current_token_payload),
-) -> UserAuthJWTSchema:
-    validate_token_type(payload=payload, token_type=REFRESH_TOKEN_TYPE)
-    return get_user_by_token_sub(payload)
+class UserGetterFromToken:
+    def __init__(self, token_type: str):
+        self.token_type = token_type
+
+    def __call__(self, payload: dict = Depends(get_current_token_payload)) -> UserAuthJWTSchema:
+        validate_token_type(payload=payload, token_type=self.token_type)
+        return get_user_by_token_sub(payload)
+
+
+# get_current_auth_user = UserGetterFromToken(ACCESS_TOKEN_TYPE)
+get_current_auth_user = get_auth_user_from_token_of_type(ACCESS_TOKEN_TYPE)
+
+get_current_auth_user_for_refresh = UserGetterFromToken(REFRESH_TOKEN_TYPE)
 
 
 def get_current_active_auth_user(
     user: UserAuthJWTSchema = Depends(get_current_auth_user),
-):
+) -> UserAuthJWTSchema:
     if user.active:
         return user
     logger.debug("User is not active: %s", user)
