666b3e74ff98e2666f3f79e310d38e8f6ce944d4
gas <pankot222@gmail.com>
2024-11-22 15:48:39 +0500

    upd: config.py try

--- src/config.py
+++ src/config.py
@@ -80,8 +80,8 @@ class AuthJWT(BaseModel):
     private_key_path: Path = BASE_DIR / "auth_jwt/certs/jwt-private.pem"
     public_key_path: Path = BASE_DIR / "auth_jwt" / "certs" / "jwt-public.pem"
     algorithm: str = "RS256"
-    access_token_expires_minutes: int = 60
-    refresh_token_expires_days: int = 15
+    access_token_expires_minutes: int = 1
+    refresh_token_expires_days: int = 120
 
 
 class Settings(BaseSettings):
