88009c97ae0b0309d346b96b8418fa9c243e6dce
gas <pankot222@gmail.com>
2024-11-30 19:36:38 +0500

    upd: change time in config.py for access v1`

--- src/config.py
+++ src/config.py
@@ -80,7 +80,7 @@ class AuthJWT(BaseModel):
     private_key_path: Path = BASE_DIR / "auth_jwt/certs/jwt-private.pem"
     public_key_path: Path = BASE_DIR / "auth_jwt" / "certs" / "jwt-public.pem"
     algorithm: str = "RS256"
-    # access_token_expires_minutes: int = 15
+    # access_token_expires_minutes: int = 15 #1
     # refresh_token_expires_days: int = 2
     access_token_expires_minutes: int = 1
     refresh_token_expires_minutes: int = 2
